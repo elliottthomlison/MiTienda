92a0f016e867f20a5d48df6e99aa23fed64bbd8c
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-04-23 16:50:57 -0700

    Refactor the loading animation and products page

--- src/components/loading.js
+++ src/components/loading.js
@@ -3,7 +3,6 @@ import FadeIn from "react-fade-in";
 import Lottie from "react-lottie";
 import "bootstrap/dist/css/bootstrap.css";
 import * as legoData from "./legoloading.json";
-import * as doneData from "./doneloading.json";
 
 const defaultOptions = {
   loop: true,
@@ -13,14 +12,7 @@ const defaultOptions = {
     preserveAspectRatio: "xMidYMid slice"
   }
 };
-const defaultOptions2 = {
-  loop: false,
-  autoplay: true,
-  animationData: doneData.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
-};
+
 
 export default class Loading extends React.Component {
   constructor(props) {
@@ -38,9 +30,9 @@ export default class Loading extends React.Component {
           this.setState({ loading: true });
           setTimeout(() => {
             this.setState({ done: true });
-          }, 1000);
+          }, 800);
         });
-    }, 2000);
+    }, 800);
   }
 
   render() {
@@ -50,15 +42,14 @@ export default class Loading extends React.Component {
           <FadeIn>
             <div class="d-flex justify-content-center align-items-center">
               {!this.state.loading ? (
-                <Lottie options={defaultOptions} height={1000} width={700} />
+                <Lottie options={defaultOptions} height={1100} width={1000} />
               ) : (
-                <Lottie options={defaultOptions2} height={1000} width={1000} />
+                <Lottie options={defaultOptions} height={1100} width={1000} />
               )}
             </div>
           </FadeIn>
         ) : (
-          <h1>hello world</h1>
-        )}
+      <td></td>        )}
       </div>
     );
   }

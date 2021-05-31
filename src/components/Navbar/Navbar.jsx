ac9b14f60507bfc005aaaca85ce56f061aa7e11e
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-31 12:49:38 -0700

    Change the styling of the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -35,7 +35,7 @@ const PrimarySearchAppBar = ({ totalItems }) => {
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
           <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
-            <img src={logo} alt="commerce.js" height="25px" className={classes.image} /> MiTienda
+            <img src={logo} alt="commerce.js" height="50px" className={classes.image} /> MiTienda
           </Typography>
           <div className={classes.grow} />
           {location.pathname === '/' && (
